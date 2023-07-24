9fb27e359b2ed12c3f1f43f5cbb664b34b62a1ff
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-07-24 23:20:34 +0530

    array file added

--- _03arrays/jagged.java
+++ _03arrays/jagged.java
@@ -1,2 +1,16 @@
+//trying out a scenario in movie ticket counter,
+//considering number of counters as number of rows,
+//we dont know number of customers standing at each counter,
+//number of tickets booked by each customer.
+
+//We can infer the total amount to be paid by the customer,
+//total revenue of all billing counters,
+//most tickets booked in a counter, to choose star performer of the day
+
+import java.util.Scanner;
 public class jagged {
+    public static void main(String [] args)
+    {
+
+    }
 }
