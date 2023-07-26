001be86caa908a2dfe596a81c927bfcb9bd8132a
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-07-26 23:43:30 +0530

    constructor

--- _04oop/constr.java
+++ _04oop/constr.java
@@ -5,11 +5,30 @@ class user{
 
     public user()
     {
-        System.out.println("constructor called");
+       // System.out.println("constructor called");
+        age = 18;
+        name = "John Doe";
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 public class constr {
     public static void main(String[] args) {
         user us1 = new user();
+        System.out.println(us1.getAge() + " : " + us1.getName());
     }
 }
