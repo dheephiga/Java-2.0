a0c93b8490efa2cde4bdfb7982fb835c6fc0c945
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-07-26 20:23:57 +0530

    encapsulation

--- _04oop/encap.java
+++ _04oop/encap.java
@@ -1,15 +1,23 @@
 class details{
     String name;
-    int age;
+    private int age;
 
-    public void
+    public void setAge(int a){
+        age = a;
+    }
+
+    public void show(details o){
+        System.out.println(o.name);
+        System.out.println(o.age);
+    }
 }
 
 public class encap {
     public static void main(String [] args)
     {
         details d1 = new details();
-        d1.age = 34;
-        d1.name = "D"
+    d1.setAge(34);
+        d1.name = "D";
+        d1.show(d1);
     }
 }
