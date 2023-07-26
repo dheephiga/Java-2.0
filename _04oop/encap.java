8d1ac3b583822310293ca1151d8316441e168684
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-07-26 20:11:00 +0530

    oop

---
+++ _04oop/encap.java
@@ -0,0 +1,15 @@
+class details{
+    String name;
+    int age;
+
+    public void
+}
+
+public class encap {
+    public static void main(String [] args)
+    {
+        details d1 = new details();
+        d1.age = 34;
+        d1.name = "D"
+    }
+}
