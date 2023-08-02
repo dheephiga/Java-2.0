5942ec2c88ee9ccb92c7be3fc687811ae028eb02
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-02 12:05:42 +0530

    practice files updated

--- practice/forr.java
+++ practice/forr.java
@@ -5,6 +5,28 @@ public class forr{
 	{
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		
+
+		int [] arr = new int [n];
+		int [] b = new int[n];
+
+		int count =0;
+		for(int i = 0 ;i<n;i++)
+			arr[i] = in.nextInt();
+
+		for(int i = 0;i <n;i++)
+		{
+			for(int j = i;j>=0;j--)
+			{
+				if(arr[j] > arr[i])
+					count++;
+			}
+			b[i] = count;
+			count =0;
+		}
+		for(int i = 0;i<n;i++)
+			System.out.print(b[i] + " ");
+
 	}
 }
+
+//9 8 7 4 3 4 1
