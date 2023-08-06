11f7220e2dd4995abcc1a457578c02acbef77251
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-06 22:19:44 +0530

    method overriding

--- _05inheritance/method_override.java
+++ _05inheritance/method_override.java
@@ -6,10 +6,10 @@ class parent{
 }
 
 class child extends parent{
-    public int sum2(int a, int b)
-    {
-        return a+b+1;
-    }
+//    public int sum2(int a, int b)
+//    {
+//        return a+b+1;
+//    }
 } // prints 8 since it overrides the parent function, if commented -> will produce input as 7
 public class method_override {
     public static void main(String[] args) {
