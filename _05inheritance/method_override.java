ca2d123a21d360dbdb45f8a601e4a3b2c3521623
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-06 22:15:13 +0530

    method overriding

--- _05inheritance/method_override.java
+++ _05inheritance/method_override.java
@@ -5,8 +5,15 @@ class parent{
     }
 }
 
-class child{
-
+class child extends parent{
+    public int sum2(int a, int b)
+    {
+        return a+b+1;
+    }
 }
 public class method_override {
+    public static void main(String[] args) {
+        child summing = new child();
+        summing.sum2(5,2);
+    }
 }
