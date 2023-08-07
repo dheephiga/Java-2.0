fdfa4545dbcf56d37a0d070e9b388f3f4fb93329
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-07 19:34:58 +0530

    dynamic-method-dispatch

--- _05inheritance/dynamic.java
+++ _05inheritance/dynamic.java
@@ -1,2 +1,24 @@
+class a{
+    public void show()
+    {
+        System.out.println("In A class");
+    }
+}
+
+class b extends  a{
+    public void show(){
+        System.out.println("In B class");
+    }
+}
 public class dynamic {
+    public static void main(String[] args) {
+        a obj = new a();
+        //here what we do is create an object of type a and object is a
+        obj.show(); //must print In A class
+
+        obj = new b();
+        // we can map the reference variable to another object
+
+
+    }
 }
