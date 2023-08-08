9ececd6d70a2dc901288d6efdd37b1b07b04269f
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-08 21:44:15 +0530

    final-keyword

--- _05inheritance/final_w.java
+++ _05inheritance/final_w.java
@@ -1,6 +1,14 @@
 final class fin{
     //this class cannot be inherited anymore
+    public void fun()
+    {
+        System.out.println("Just a method");
+    }
 }
+
+//class fine extends fin{
+//    //throws error over here, once you uncomment it
+//}
 public class final_w {
     public static void main(String[] args) {
         //final variable
@@ -9,7 +17,6 @@ public class final_w {
 
         final int ar = 3948;
       //  ar = 93839; //throws  error
-
 
     }
 }
