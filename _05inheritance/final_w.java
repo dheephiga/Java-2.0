e2e4d324db38b944079e24691fe857dc71d9b7ae
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-08 21:10:46 +0530

    final-keyword

--- _05inheritance/final_w.java
+++ _05inheritance/final_w.java
@@ -1,5 +1,15 @@
+final class fin{
+    //this class cannot be inherited anymore
+}
 public class final_w {
     public static void main(String[] args) {
         //final variable
+        int var = 0;
+        var = 800; //no error
+
+        final int ar = 3948;
+      //  ar = 93839; //throws  error
+
+
     }
 }
