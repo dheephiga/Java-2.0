d7fd3b85fb08670615b61ff289d3325d1544a9e4
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-17 12:54:19 +0530

    adding files

--- practice/pw.java
+++ practice/pw.java
@@ -4,6 +4,23 @@ public class pw {
         Scanner in = new Scanner(System.in);
         String pass = in.next();
 
+            if((pass.charAt(0) <= '9') && (pass.charAt(0) >= '0') || (pass.length()<4))
+                System.out.println("no");
+
+            else {
+
+                for (int i = 0; i < pass.length(); i++)
+                {
+                    if((pass.charAt(i) <= '9') && (pass.charAt(i) >= '0'))
+                        num++;
+                    if(pass.charAt(i) <= 'Z' || pass.charAt(i) >= 'A')
+                        cap++;
+
+                }
+            }
+
+
+
 
     }
 }
