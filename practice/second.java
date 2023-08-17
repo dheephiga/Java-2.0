7a7aeff1a29d636d1a2fb2bd5d99e6edb5521b15
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-17 12:09:52 +0530

    adding files

--- practice/second.java
+++ practice/second.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class second {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList <Integer>  even = new ArrayList<Integer>();
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+
+        int [] maj = new int[n];
+
+
+    }
 }
