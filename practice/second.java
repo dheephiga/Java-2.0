ac1c7acec4d0dcb8588ceb461a049d06f3f854ce
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-17 12:13:12 +0530

    adding files

--- practice/second.java
+++ practice/second.java
@@ -8,6 +8,21 @@ public class second {
 
         int [] maj = new int[n];
 
+        for(int i:maj)
+            maj[i] = in.nextInt();
 
+        for(int i = 0;i<n;i++)
+        {
+            if(i%2==0)
+                even.add(maj[i]);
+            else
+                odd.add(maj[i]);
+        }
+        Collections.sort(odd);
+        Collections.sort(even);
+        int oddsec = odd.get(odd.size()-2);
+        int evensec = even.get(even.size()-2);
+
+        System.out.println(oddsec+evensec);
     }
 }
