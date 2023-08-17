b15cd1d626d07fb0468fa0d49809df96360deae1
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-17 13:25:21 +0530

    adding files

---
+++ practice/unique.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class unique {
+    public static void main(String[] args) {
+
+                int arr[] = {1, 5, 9, 1, 4, 9, 6, 5, 9, 7};
+                int n = arr.length;
+                int i, j;
+                int count=0;
+//                System.out.print("The array is: ");
+//                for (i = 0; i < n; ++i)
+//                    System.out.print(arr[i] + " ");
+                System.out.print("The distinct elements of above array are: ");
+                for (i = 0; i < n; i++) {
+                    for (j = 0; j < i; j++)
+                        if (arr[i] == arr[j])
+                            break;
+                    if (i == j)
+                        count++;
+                }
+        System.out.println(count);
+            }
+        }
