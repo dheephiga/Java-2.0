9d608a8a54645912e9afd3c86bfae57a5a550cf4
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-18 13:04:22 +0530

    adding files

--- practice/sort.java
+++ practice/sort.java
@@ -5,15 +5,15 @@ public class sort {
         Scanner in = new Scanner(System.in);
         ArrayList <Integer> list = new ArrayList <Integer> ();
         int flag =0;
-        for(int n=0;n<8;n++)
+        for(int n=0;n<=arr.length-1;n++)
         {
             arr[n] = in.nextInt();
             list.add(arr[n]);
         }
         Collections.sort(list);
-        for(int i=1;i<=10;i++)
+        for(int i=1;i<=arr.length+1;i++)
         {
-            if(i== list.get(i))
+            if(i== list.get(i-1))
                 flag++;
             else
                 System.out.println(i);
