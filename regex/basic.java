459cc71343f019768d359b6658acc3eb9dfa7fb3
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-23 12:51:43 +0530

    adding files

---
+++ regex/basic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class basic {
+    public static void main(String[] args) {
+        String s = "hello";
+        Pattern pattern = Pattern.compile(s);
+    }
+}
+
