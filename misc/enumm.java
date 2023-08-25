012aa3093b9d7619962c7e6b1126d60b0fec8e1f
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-25 18:27:51 +0530

    adding files

--- misc/enumm.java
+++ misc/enumm.java
@@ -4,6 +4,9 @@ enum status {
 public class enumm {
     public static void main(String[] args) {
         int i = 8;
-
+        status [] s = status.values();
+        System.out.println(s);
+        for(status a : s)
+            System.out.println(s[a]);
     }
 }
