2e75cd0fb0ee0ea9d4f006f903ffad9227162e5c
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-26 23:05:01 +0530

    adding files

--- exception/excephandling.java
+++ exception/excephandling.java
@@ -1,5 +1,11 @@
 public class excephandling {
     public static void main(String[] args) {
-
+            try{
+                int res = 13/0;
+            }
+            catch (Exception e)
+            {
+                System.out.println("Cannot be done");
+            }
     }
 }
