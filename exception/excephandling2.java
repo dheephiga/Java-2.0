f6f5c457ad18c36f90cd025a32c859e33ccd5c63
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-27 10:02:34 +0530

    adding files

--- exception/excephandling2.java
+++ exception/excephandling2.java
@@ -1,5 +1,14 @@
 public class excephandling2 {
     public static void main(String[] args) {
+            try{
+                int i = 19/0;
+                if(i==0)
+                throw new ArithmeticException("Cannot divide by zero");
+            }
 
+            catch(ArithmeticException e)
+            {
+
+            }
     }
 }
