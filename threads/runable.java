460515e821e30898205252b906982466a35a2784
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-01 11:41:21 +0530

    modified runable.java

--- threads/runable.java
+++ threads/runable.java
@@ -1,42 +1,65 @@
-import java.awt.*;
+//class ar implements Runnable{
+//    public void run() {
+//        for(int i =0;i<5;i++)
+//            System.out.println("hello");
+//        try {
+//            Thread.sleep(2);
+//        }
+//        catch(InterruptedException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+//}
 
-class ar implements Runnable{
-    public void run() {
-        for(int i =0;i<5;i++)
-            System.out.println("hello");
-        try {
-            Thread.sleep(2);
-        }
-        catch(InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-    }
-}
-
-class br implements Runnable{
-    public void run()
-    {
-        for (int i = 0;i<5;i++) {
-            System.out.println("Hi");
-            try{
-                Thread.sleep(10);
-            }
-            catch(InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-    }
-}
+//class br implements Runnable{
+//    public void run()
+//    {
+//        for (int i = 0;i<5;i++) {
+//            System.out.println("Hi");
+//            try{
+//                Thread.sleep(10);
+//            }
+//            catch(InterruptedException e)
+//            {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
+//}
 public class runable {
     public static void main(String[] args) {
-        ar obj1 = new ar();
-        br obj2 = new br();
+       Runnable obj1 = new Runnable() {
+           public void run() {
+               for(int i = 0; i < 5; i++)
+               {
+                   System.out.println("Hello");
 
-        Thread t1 = new Thread();
-        Thread t2 = new Thread();
+                   try{Thread.sleep(2); } catch(InterruptedException e) { e.printStackTrace();}
+               }
+           }
+       };
+
+       Runnable obj2 = () -> {
+
+               for(int i =0;i<5;i++)
+               {
+                   System.out.println("hi");
+                   try{
+                       Thread.sleep(5);
+                   }
+                   catch(InterruptedException e)
+                   {
+                       e.printStackTrace();
+                   }
+               }
+       };
+       //ar obj1 = new ar();
+       //br obj2 = new br();
+//
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
 
         t1.start();
         t2.start();
